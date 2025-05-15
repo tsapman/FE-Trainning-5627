23eaa55814c04a63f50bf7ddcd6cbed7816e721c
panagiotis.tsaparas@clarivate.com <panagiotis.tsaparas@clarivate.com>
2025-05-15 10:02:53 +0200

    deleting comments

--- src/app/top-footer-buttons/top-footer-buttons.component.ts
+++ src/app/top-footer-buttons/top-footer-buttons.component.ts
@@ -29,7 +29,7 @@ filterPhotos(albumIdRange: [number, number]): void {
 }
 
 showFilteredPhotos(albumIdRange: [number, number]): void {
-    const [start, end] = albumIdRange; // Destructure the range
+    const [start, end] = albumIdRange; 
     const albumMap = new Map<number, Photos>();
     this.photos
         .filter((photo: Photos) => photo.albumId >= start && photo.albumId <= end)
@@ -45,13 +45,13 @@ showFilteredPhotos(albumIdRange: [number, number]): void {
 showAllPhotos(): void {
   const albumMap = new Map<number, Photos>();
   this.photos
-    .filter((photo: Photos) => photo.albumId >= 1 && photo.albumId <= 100) // Filter album IDs 1 to 100
+    .filter((photo: Photos) => photo.albumId >= 1 && photo.albumId <= 100) 
     .forEach((photo: Photos) => {
       if (!albumMap.has(photo.albumId)) {
         albumMap.set(photo.albumId, photo);
       }
     });
-  this.filteredPhotos = Array.from(albumMap.values()); // Include all filtered photos
-  this.clickButtonEvent.emit(this.filteredPhotos); // Emit the filtered photos
+  this.filteredPhotos = Array.from(albumMap.values()); 
+  this.clickButtonEvent.emit(this.filteredPhotos); 
 }
 }
