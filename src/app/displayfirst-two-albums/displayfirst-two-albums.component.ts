e9166bc991da01f50996e6014c4ea0d6dff6856b
panagiotis.tsaparas@clarivate.com <panagiotis.tsaparas@clarivate.com>
2025-05-18 19:05:57 +0200

    styling first component

--- src/app/displayfirst-two-albums/displayfirst-two-albums.component.ts
+++ src/app/displayfirst-two-albums/displayfirst-two-albums.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-displayfirst-two-albums',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './displayfirst-two-albums.component.html',
   styleUrl: './displayfirst-two-albums.component.scss'
 })
