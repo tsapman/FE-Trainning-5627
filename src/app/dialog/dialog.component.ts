13ce2e84ffdcf668dfbde116872f13309bf9741a
panagiotis.tsaparas@clarivate.com <panagiotis.tsaparas@clarivate.com>
2025-05-20 12:47:19 +0200

    creating mat dialog

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -2,14 +2,41 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { DataService } from '../service/data.service';
+import { CommonModule } from '@angular/common';
+import { Photos } from '../photos';
+
+
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [MatButtonModule,MatIconModule,MatDialogModule],
+  imports: [CommonModule,MatButtonModule,MatIconModule,MatDialogModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent {
+  public photos : Photos [] = [];
+  public filteredPhotos : Photos [] = [];
+  errorMessage = '';
+
+  constructor(private data_service : DataService) {
+  }
+
+  ngOnInit(): void {
+    this.data_service.getAllPhotos().subscribe({
+      next: (photos : Photos[]) => {
+        this.photos = photos;
+        this.filteredPhotos = this.photos.filter((photo : Photos) => photo.albumId === 1);
+    },
+      error: (error : any) => {
+        this.errorMessage = error;
+        console.error('Error fetching photos:', error);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {}
+
   
 }
