a0641eb8345b9d52ce5607a630d1231725f25e26
panagiotis.tsaparas@clarivate.com <panagiotis.tsaparas@clarivate.com>
2025-05-23 15:24:29 +0200

    making elements responsive and styling backround colours

--- src/app/first-album/first-album.component.ts
+++ src/app/first-album/first-album.component.ts
@@ -44,7 +44,7 @@ export class FirstAlbumComponent {
   ngOnDestroy(): void {}
 
   openDialogToShowId(photo : Photos): void {
-    this._matDialog.open(DialogComponent, {width : "400px", height : "475px", data: photo} )
+    this._matDialog.open(DialogComponent, {width : "auto", height : "475px", data: photo} )
   }
 
   
