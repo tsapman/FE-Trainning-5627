b7d4313d74af4f83b26a9bc7bb4c51c1b3fa6d96
panagiotis.tsaparas@clarivate.com <panagiotis.tsaparas@clarivate.com>
2025-05-26 12:58:27 +0200

    creating input form

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -7,11 +7,14 @@ import { SnackbarComponent } from '../snackbar/snackbar.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TopFooterButtonsComponent } from '../top-footer-buttons/top-footer-buttons.component';
 import { DisplayfirstTwoAlbumsComponent } from '../displayfirst-two-albums/displayfirst-two-albums.component';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-display',
   standalone: true,
-  imports: [CommonModule,ImagesComponent,SnackbarComponent,TopFooterButtonsComponent,DisplayfirstTwoAlbumsComponent],
+  imports: [CommonModule,ImagesComponent,SnackbarComponent,TopFooterButtonsComponent,DisplayfirstTwoAlbumsComponent, MatIcon, FormsModule, MatLabel,MatFormField],
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss'],
 })
@@ -20,6 +23,7 @@ export class DisplayComponent implements OnInit {
   public filteredPhotos: Photos[] = []; 
   public errorMessage: string = '';
   public showAllAlbums: boolean = true;
+  public value: string = '';
 
   constructor(private data_service: DataService, private snackBar : MatSnackBar ) {}
 
